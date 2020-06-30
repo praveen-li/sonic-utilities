8ed70d426b88fe65547ae4cb4281fcc62e6f56d0
Praveen Chaudhary <pchaudhary@linkedin.com>
2020-06-30 14:20:29 -0700

    [config/config_mgmt.py]: Remove unneccessary print.
    Signed-off-by: Praveen Chaudhary pchaudhary@linkedin.com

--- config/config_mgmt.py
+++ config/config_mgmt.py
@@ -651,7 +651,6 @@ class ConfigMgmtDPB(ConfigMgmt):
 
             elif isinstance(outp, list):
                 # just take list from output
-                print("insert in List: {} {} {}".format(inp, outp, diff))
                 config.extend(outp)
                 # in config DB, empty lists are represented by [""]
                 if "" in config:
